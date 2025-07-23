37722b29303ba1316b17a908cff15504aeebecb4
izfae21 <izfae.21@gmail.com>
2025-07-23 13:21:49 +0800

    Update code.py and added temperature

--- code.py
+++ code.py
@@ -5,3 +5,12 @@ height = 1.75   # Float
 is_student = True # Boolean
 
 print(f"Name: {name}, Age: {age}, Height: {height}, Student: {is_student}")
+
+temperature = 25
+
+if temperature > 30:
+    print("It's hot outside!")
+elif temperature < 10:
+    print("It's cold outside!")
+else:
+    print("The weather is mild.")

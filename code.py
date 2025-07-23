e23b93838a1c08a5b7f83684e84e6a9634e5141e
izfae21 <izfae.21@gmail.com>
2025-07-23 13:19:54 +0800

    Update code.py and added color

--- code.py
+++ code.py
@@ -1,1 +1,7 @@
 print("Hello, World!")
+name = "Izzaty"  # String
+age = 30        # Integer
+height = 1.75   # Float
+is_student = True # Boolean
+
+print(f"Name: {name}, Age: {age}, Height: {height}, Student: {is_student}")
